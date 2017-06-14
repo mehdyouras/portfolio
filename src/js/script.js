5e8ba2fd70f96ccaa90792d8e749645e24041d4d
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-06-14 09:34:19 +0200

    Added small screen navbar

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+const openNav = function(oEvent) {
+    oEvent.preventDefault();
+    let $nav = document.querySelector(".nav");
+    $nav.style.transform = "translatex(0)";
+    document.addEventListener("click", function(oEvent) {
+        if(oEvent.pageX > 250) {
+            closeNav(oEvent);
+        }
+    });
+    document.onkeydown = function(evt) {
+        evt = evt || window.event;
+        if (evt.keyCode == 27) {
+            closeNav(evt);
+        }
+    };
+}
+const closeNav = function(oEvent) {
+    oEvent.preventDefault();
+    document.querySelector(".nav").style.transform = "translatex(-250px)";
+}
+
+const fPageIsLoaded = function() {
+    document.querySelector(".hamburger").addEventListener("click", openNav);
+}
+
+window.addEventListener("load", fPageIsLoaded);
