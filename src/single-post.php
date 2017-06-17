9c2f124cf612b8917ae0c630acb481ca226e9a27
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-06-17 14:43:03 +0200

    Finished single post

--- src/single-post.php
+++ src/single-post.php
@@ -1,35 +1,25 @@
 <?php get_header(); ?>
     <section class="main-wrapper">
-        <h2 class="u-hidden-visually">Contenu principal</h2>
+        <h2 class="u-hidden-visually"><?php the_title(); ?></h2>
         <div class="o-list-bare card-wrapper">
             <div class="card card_no-padding">
                 <header class="card__header">
-                    <div class="tags">
-                        <span class="tags__item">Front-end</span>
-                        <span class="tags__item">HTML</span>
-                        <span class="tags__item">CSS</span>
-                        <span class="tags__item">JS</span>
-                    </div>
-                    <a href="post.html" class="card__title">à la découverte de ReactJS</a>
-                    <time datetime="2017-09-12">12/09/2017</time>
+                    <?php if(get_field('post_themes')) : ?>
+                        <div class="tags">
+                            <?php foreach(get_field('post_themes') as $theme) : ?>
+                                <span class="tags__item"><?= $theme->name ?></span>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                    <a href="<?php the_permalink(); ?>" class="card__title"><?php the_title(); ?></a>
+                    <time datetime="2017-09-12"><?php the_field('post_date'); ?></time>
                 </header>
             </div>
         </div>
-        <article class="card-wrapper">
-            <h3 class="u-hidden-visually">A la découverte de ReactJS</h3>
-            <div class="card">
-                <div class="wysiwyg">
-                    <h3 class="u-blue">Lourd le mec</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste laboriosam magni numquam officiis suscipit! Accusamus ad aliquam atque autem dolores facere, ipsam officia omnis quae reiciendis ullam veniam vero voluptatibus!</p>
-                </div>
-                <figure class="figure">
-                    <img class="figure__image" src="img/works/artiste.jpg" alt="Test">
-                    <figcaption class="figure__caption">Larry Burrows paramètrant son appareil photo</figcaption>
-                </figure>
-                <div class="wysiwyg">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste laboriosam magni numquam officiis suscipit! Accusamus ad aliquam atque autem dolores facere, ipsam officia omnis quae reiciendis ullam veniam vero voluptatibus!</p>
-                </div>
-            </div>
-        </article>
+        <?php if(have_rows('flexible')) : ?>
+            <article class="card-wrapper">
+                <?php get_template_part('part','flexible'); ?>
+            </article>
+        <?php endif; ?>
     </section>
 <?php get_footer(); ?>
