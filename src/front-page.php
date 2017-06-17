1b0973b3a3a7573694645732fe96afc2ec041a78
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-06-17 11:49:44 +0200

    Finished front-page

--- src/front-page.php
+++ src/front-page.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 <section class="main-wrapper">
-    <h2 class="u-hidden-visually">Contenu principal</h2>
+    <h2 class="u-hidden-visually"><?= __('Contenu principal', 'pf') ?></h2>
     <section class="introBlock">
         <?php pf_the_image(get_field('mehdy_photo', 'options'), 'pf_content_thin', false, false); ?>
         <h3 class="introBlock__title"><?php the_field('mehdy_name', 'options'); ?></h3>
@@ -12,29 +12,22 @@ get_header();
             <span class="contactInfos__item icons_pink icons icons_phone"><?php the_field('mehdy_phone', 'options'); ?></span>
             <?php endif; ?>
         </address>
-        <a class="cta cta_lines cta_light" href="cv.html">CV</a>
+        <a class="cta cta_lines cta_light" href="<?php the_field('cta_link'); ?>"><?php the_field('cta_title'); ?></a>
     </section>
+    <?php if(have_rows('cards')) : ?>
     <div class="card-wrapper">
+        <?php while(have_rows('cards')) : the_row() ?>
         <div class="card">
             <div class="u-center-text">
-                <a class="cta cta_dark cta_lines" href="contact.html">M'engager</a>
+                <a class="cta cta_dark cta_lines" href="<?php the_sub_field('cta_link'); ?>"><?php the_sub_field('cta_title'); ?></a>
             </div>
-            <p class="blue">
-                Je suis un jeune dévelopeur web de Huy en Belgique. Je suis un passioné du dévelopement backend propre, clair et documenté.
-            </p>
-            <p class="u-blue">
-                <strong>Prêt à sauter dans votre prochain projet !</strong>
-            </p>
-        </div>
-        <div class="card">
-            <div class="u-center-text">
-                <a class="cta cta_dark cta_lines" href="portfolio.html">Portfolio</a>
+            <div class="wysiwyg">
+                <?php the_sub_field('text'); ?>
             </div>
-            <p class="u-blue">
-                J'entretiens une liste de tous mes travaux réalisés ainsi qu'une description de chacun de leurs points forts. C'est le meilleur moyen d'en apprendre plus sur ma façon de travailler.
-            </p>
         </div>
+        <?php endwhile; ?>
     </div>
+    <?php endif; ?>
 </section>
 <?php
 get_footer();
