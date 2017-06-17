9dc22d3f6c956ed42bbe58177d4ffdb85786c11b
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-06-17 15:50:11 +0200

    Finished cv

--- src/template-cv.php
+++ src/template-cv.php
@@ -5,54 +5,51 @@
 get_header();
 ?>
 <section class="main-wrapper">
-    <h2 class="u-hidden-visually">Contenu principal</h2>
+    <h2 class="u-hidden-visually"><?= __('Contenu principal', 'pf'); ?></h2>
     <section class="introBlock">
-        <img src="img/portrait.jpg" alt="Portrait de Mehdy Ouras">
-        <h3 class="introBlock__title">Mehdy Ouras</h3>
-        <address class="contactInfos">
-            <span class="contactInfos__item icons_pink icons icons_email">mehdy@ouras.be</span>
-            <span class="contactInfos__item icons_pink icons icons_phone">0471 79 85 15</span>
-        </address>
-        <a class="cta cta_lines cta_light" href="portfolio.html">Portfolio</a>
+        <?php get_template_part('part', 'mehdy'); ?>
+        <a class="cta cta_lines cta_light" href="<?php the_field('cta_link') ?>"><?php the_field('cta_title') ?></a>
     </section>
     <div class="card-wrapper">
         <div class="card">
-            <h3 class="u-hidden-visually">Mon CV</h3>
+            <h3 class="u-hidden-visually"><?= __('Mon CV', 'pf'); ?></h3>
+            <?php if( have_rows('cv_edu') ): ?>
             <ol class="o-list-bare">
+                <?php while(have_rows('cv_edu')) : the_row();?>
                 <li>
-                    <p class="cv__title cv__title_right no-margin-top icons icons_edu"><time datetime="2015">2015</time> - <time datetime="2018">2018</time></p>
+                    <p class="cv__title cv__title_right no-margin-top icons icons_edu">
+                        <time datetime="<?php the_sub_field('edu_from') ?>"><?php the_sub_field('edu_from') ?></time><?php __(' - ', 'pf'); ?>
+                        <?php if(get_sub_field('edu_to')) : ?>
+                        <time datetime="<?php the_sub_field('edu_to') ?>"><?php the_sub_field('edu_to') ?></time>
+                        <?php endif; ?>
+                    </p>
                     <p class="cv__content">
-                        Bachelier en infographie orientation web
+                        <?php the_sub_field('edu_desc') ?>
                     </p>
                 </li>
-                <li>
-                    <p class="cv__title cv__title_right icons icons_edu"><time datetime="2015">2009</time> - <time datetime="2018">2015</time></p>
-                    <p class="cv__content">
-                        Humanités générales orientation sciences et sciences économiques
-                    </p>
-                </li>
+                <?php endwhile; ?>
             </ol>
+            <?php endif; ?>
             <dl>
-                <dt class="cv__title icons icons_globe">Langues parlées</dt>
-                <dd class="cv__content">Français et anglais</dd>
+                <dt class="cv__title icons icons_globe"><?= __('Langues parlées', 'pf'); ?></dt>
+                <dd class="cv__content"><?php the_field('cv_language') ?></dd>
 
-                <dt class="cv__title icons icons_code">Langages</dt>
-                <dd class="cv__content">HTML5</dd>
-                <dd class="cv__content">CSS3</dd>
-                <dd class="cv__content">Javascript</dd>
-                <dd class="cv__content">PHP</dd>
+                <dt class="cv__title icons icons_code"><?= __('Langages', 'pf'); ?></dt>
+                <?php foreach(get_field('cv_code') as $code) : ?>
+                <dd class="cv__content"><?= $code; ?></dd>
+                <?php endforeach; ?>
             </dl>
 
             <div>
-                <p class="cv__title icons icons_game">Hobbies</p>
-                <p class="cv__content">Les technologies (nouvelles ou non), les jeux vidéos, la photographie, boire des coups avec les copains</p>
+                <p class="cv__title icons icons_game"><?= __('Hobbies', 'pf'); ?></p>
+                <p class="cv__content"><?php the_field('cv_hobbies') ?></p>
             </div>
 
             <dl class="no-margin-bottom">
-                <dt class="cv__title icons icons_plus">Extras</dt>
-                <dd class="cv__content icons icons_car">Permis B</dd>
-                <dd class="cv__content icons icons_smile">Un sourire</dd>
-                <dd class="cv__content icons icons_battery">De l'énergie à revendre</dd>
+                <dt class="cv__title icons icons_plus"><?= __('Extras', 'pf'); ?></dt>
+                <dd class="cv__content icons icons_car"><?= __('Permis B', 'pf'); ?></dd>
+                <dd class="cv__content icons icons_smile"><?= __('Un sourire', 'pf'); ?></dd>
+                <dd class="cv__content icons icons_battery"><?= __('De l\'énrgie à revendre', 'pf'); ?></dd>
             </dl>
         </div>
     </div>
